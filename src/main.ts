bec2df6c4b5034008821a16672a90f8a075cea8c
Rarity110 <rarity110@mail.ru>
2023-02-06 01:08:21 +0300

    feat: add ability of changing PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
+const PORT = config().parsed?.PORT ? config().parsed?.PORT : 4000;
 async function bootstrap() {
-  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
