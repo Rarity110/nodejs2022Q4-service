cc96bf13c541cd6ab9224635368598c01c350406
Rarity110 <rarity110@mail.ru>
2023-02-06 13:21:32 +0300

    feat: add building yalm doc

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { stringify } from 'yaml';
+import * as fs from 'node:fs';
 
 const PORT = config().parsed?.PORT ? config().parsed?.PORT : 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
     .setDescription('Home music library service')
     .setVersion('1.0')
-    .addTag('user')
+    .addServer(`http://localhost:${PORT}`)
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+  const yalmString: string = stringify(document, {});
+  fs.writeFileSync('./doc/api.yalm', yalmString, { encoding: 'utf-8' });
   SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
